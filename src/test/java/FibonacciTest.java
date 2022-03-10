3094dd397dc5f4ac97891034f91d73b80a6a54bf
jesu-m0 <jesusmd2000@gmail.com>
2022-03-10 12:08:17 +0100

    Creation of tests

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,5 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
 public class FibonacciTest {
 
+    @Test
+    public void negativeNumber() {
+        assertThrows(RuntimeException.class, () -> Fibonacci.compute(-1));
+    }
 
+    @Test
+    public void validResultNumber0() {
+        assertEquals(0, Fibonacci.compute(0));
+    }
+
+    @Test
+    public void validResultNumber1() {
+        assertEquals(1, Fibonacci.compute(1));
+    }
+
+    @Test
+    public void validResultNumber2() {
+        assertEquals(1, Fibonacci.compute(2));
+    }
+
+    @Test
+    public void validResultNumber5() {
+        assertEquals(5, Fibonacci.compute(5));
+    }
 
 }
