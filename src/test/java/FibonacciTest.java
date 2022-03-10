445b64991e599834247bad7aedd48aab32ceb0df
jesu-m0 <jesusmd2000@gmail.com>
2022-03-10 12:07:04 +0100

    Errors corrections on Fibonacci.java

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,17 +1,5 @@
 public class FibonacciTest {
 
-    //Calcula de manera recursiva el resultado
-    public static int compute(int numero){
 
-        if(numero < 0){
-            throw new RuntimeException("El numero de ser mayor o igual a 0");
-        } else if(numero==0){
-            return 0;
-        }else if(numero==1){
-            return 1;
-        }else{
-            return compute(numero-1) + compute(numero-2);
-        }
-    }
 
 }
